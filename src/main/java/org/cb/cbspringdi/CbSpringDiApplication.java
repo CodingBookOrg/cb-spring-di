246f3855809f6299639d7bf5d51ab75636e3cb09
orhan <orhanu2020@gmail.com>
2020-11-20 00:21:18 -0500

    Initial project structure

---
+++ src/main/java/org/cb/cbspringdi/CbSpringDiApplication.java
@@ -0,0 +1,13 @@
+package org.cb.cbspringdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbSpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbSpringDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cb/cbspringdi/CbSpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cb.cbspringdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbSpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
